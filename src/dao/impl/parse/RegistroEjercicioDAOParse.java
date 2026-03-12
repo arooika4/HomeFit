3f621f5b1cb6cf8fd7c68aa57c86466a9986408c
arooika4 <aroamilagros2005@gmail.com>
2026-03-12 18:18:36 +0100

    Update RegistroEjercicioDAOParse.java

--- src/dao/impl/parse/RegistroEjercicioDAOParse.java
+++ src/dao/impl/parse/RegistroEjercicioDAOParse.java
@@ -2,46 +2,56 @@ package dao.impl.parse;
 
 import dao.RegistroEjercicioDAO;
 import model.RegistroEjercicio;
+import service.Back4AppConnection;
 
-import java.util.List;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
 import java.util.ArrayList;
+import java.util.List;
 
 public class RegistroEjercicioDAOParse implements RegistroEjercicioDAO {
 
     @Override
     public void crear(RegistroEjercicio registro) {
 
-        System.out.println("Guardando registro de ejercicio");
+        try {
+
+            HttpURLConnection conn = Back4AppConnection.conectar("RegistroEjercicio", "POST");
+
+            String json =
+                    "{ \"tiempo\":" + registro.getTiempo() +
+                    ",\"nivel\":\"" + registro.getNivel() + "\"" +
+                    ",\"foto\":\"" + registro.getFoto() + "\"" +
+                    "}";
+
+            OutputStream os = conn.getOutputStream();
+            os.write(json.getBytes());
+            os.flush();
+
+            System.out.println("Registro guardado en Back4App");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 
     @Override
     public RegistroEjercicio obtener(int id) {
-
-        System.out.println("Obteniendo registro");
-
         return null;
     }
 
     @Override
     public List<RegistroEjercicio> obtenerTodos() {
 
-        System.out.println("Listando registros");
+        System.out.println("GET registros");
 
         return new ArrayList<>();
     }
 
     @Override
-    public void actualizar(RegistroEjercicio registro) {
-
-        System.out.println("Actualizando registro");
-
-    }
+    public void actualizar(RegistroEjercicio registro) {}
 
     @Override
-    public void eliminar(int id) {
-
-        System.out.println("Eliminando registro");
-
-    }
+    public void eliminar(int id) {}
 }
