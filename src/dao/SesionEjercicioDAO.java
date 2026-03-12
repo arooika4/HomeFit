0f850ae47dcdea85fa0b58fcb4c5228e4405f9ce
arooika4 <aroamilagros2005@gmail.com>
2026-03-12 17:21:28 +0100

    Create SesionEjercicioDAO.java

---
+++ src/dao/SesionEjercicioDAO.java
@@ -0,0 +1,17 @@
+package dao;
+
+import java.util.List;
+import model.RegistroEjercicio;
+
+public interface RegistroEjercicioDAO {
+
+    void crear(RegistroEjercicio registro);
+
+    RegistroEjercicio obtener(int id);
+
+    List<RegistroEjercicio> obtenerTodos();
+
+    void actualizar(RegistroEjercicio registro);
+
+    void eliminar(int id);
+}
