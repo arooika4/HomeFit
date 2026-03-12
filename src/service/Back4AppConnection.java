84a4ac43442fe3c60996daec7372020e9c04d078
arooika4 <aroamilagros2005@gmail.com>
2026-03-12 18:24:23 +0100

    Update Back4AppConnection.java

--- src/service/Back4AppConnection.java
+++ src/service/Back4AppConnection.java
@@ -2,8 +2,8 @@ package service;
 
 public class Back4AppConnection {
 
-    public static final String APPLICATION_ID = "TU_APP_ID";
-    public static final String REST_API_KEY = "TU_REST_KEY";
+    public static final String APPLICATION_ID = "V6pp2LfU6ZLO3X5O9ZMzpzsArgN29iX7r9FbENXD";
+    public static final String REST_API_KEY = "O4zVPA8wNkgSQVcYgd1E6BUbj5fZAJXs7p73gv9W";
     public static final String API_URL = "https://parseapi.back4app.com/classes/";
 
 }
