808d3a2147d914609d6dd7ec30ce0bf0d4161862
arooika4 <aroamilagros2005@gmail.com>
2026-03-12 17:22:34 +0100

    Create RutinaDAOCollection.java

---
+++ src/dao/impl/RutinaDAOCollection.java
@@ -0,0 +1,41 @@
+package daoimpl;
+
+import dao.RutinaDAO;
+import model.Rutina;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RutinaDAOCollection implements RutinaDAO {
+
+    private List<Rutina> rutinas = new ArrayList<>();
+
+    @Override
+    public void crear(Rutina rutina) {
+        rutinas.add(rutina);
+    }
+
+    @Override
+    public Rutina obtener(int id) {
+        for (Rutina r : rutinas) {
+            if (r.getIdRutina() == id) {
+                return r;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public List<Rutina> obtenerTodas() {
+        return rutinas;
+    }
+
+    @Override
+    public void actualizar(Rutina rutina) {
+        // implementar después
+    }
+
+    @Override
+    public void eliminar(int id) {
+        // implementar después
+    }
+}
