f21c081c559819f31621bec98bbc61075cce8eb8
arooika4 <aroamilagros2005@gmail.com>
2026-03-12 17:19:30 +0100

    Create RegistroEjercicio.java

---
+++ src/model/RegistroEjercicio.java
@@ -0,0 +1,62 @@
+package model;
+
+public class RegistroEjercicio {
+
+    private int idRegistro;
+    private int idSesion;
+    private int idEjercicio;
+
+    private int tiempo;
+    private String nivel;
+    private String foto;
+
+    public RegistroEjercicio(){}
+
+    public int getIdRegistro() {
+        return idRegistro;
+    }
+
+    public void setIdRegistro(int idRegistro) {
+        this.idRegistro = idRegistro;
+    }
+
+    public int getIdSesion() {
+        return idSesion;
+    }
+
+    public void setIdSesion(int idSesion) {
+        this.idSesion = idSesion;
+    }
+
+    public int getIdEjercicio() {
+        return idEjercicio;
+    }
+
+    public void setIdEjercicio(int idEjercicio) {
+        this.idEjercicio = idEjercicio;
+    }
+
+    public int getTiempo() {
+        return tiempo;
+    }
+
+    public void setTiempo(int tiempo) {
+        this.tiempo = tiempo;
+    }
+
+    public String getNivel() {
+        return nivel;
+    }
+
+    public void setNivel(String nivel) {
+        this.nivel = nivel;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+}
