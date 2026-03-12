ba6a84657475615101cb81889417e78e49af8864
arooika4 <aroamilagros2005@gmail.com>
2026-03-12 17:00:33 +0100

    Create AppConsola.java

---
+++ app/ui/AppConsola.java
@@ -0,0 +1,54 @@
+package ui;
+
+import java.util.Scanner;
+
+public class AppConsola {
+
+    Scanner scanner = new Scanner(System.in);
+
+    public void iniciar(){
+
+        int opcion;
+
+        do{
+
+            mostrarMenu();
+            opcion = scanner.nextInt();
+
+            switch(opcion){
+
+                case 1:
+                    crearRutina();
+                    break;
+
+                case 2:
+                    listarRutinas();
+                    break;
+
+                case 3:
+                    crearSesion();
+                    break;
+
+                case 4:
+                    registrarEjercicio();
+                    break;
+
+                case 0:
+                    System.out.println("Salir");
+                    break;
+            }
+
+        }while(opcion != 0);
+
+    }
+
+    private void mostrarMenu(){
+        System.out.println("---- APP FITNESS ----");
+        System.out.println("1. Crear rutina");
+        System.out.println("2. Ver rutinas");
+        System.out.println("3. Crear sesión");
+        System.out.println("4. Registrar ejercicio");
+        System.out.println("0. Salir");
+    }
+
+}
