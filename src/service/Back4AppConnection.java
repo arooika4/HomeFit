0b59af272b3ad965358a8cbbe4059f0bf86a29b3
arooika4 <aroamilagros2005@gmail.com>
2026-03-12 17:47:55 +0100

    Create Back4AppConnection.java

---
+++ src/service/Back4AppConnection.java
@@ -0,0 +1,9 @@
+package service;
+
+public class Back4AppConnection {
+
+    public static final String APPLICATION_ID = "TU_APP_ID";
+    public static final String REST_API_KEY = "TU_REST_KEY";
+    public static final String API_URL = "https://parseapi.back4app.com/classes/";
+
+}
