bf5d526ef2db893203fc6c10941188adca78d120
Emma <emmagopro96@gmail.com>
2026-03-12 19:23:32 +0100

    Main editado
    Removed unused imports and refactored main method to use AppConsola.

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,17 +1,13 @@
-import dao.impl.parse.RutinaDAOParse;
-import model.Rutina;
+package main;
+
+import ui.AppConsola;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        RutinaDAOParse dao = new RutinaDAOParse();
-
-        Rutina r = new Rutina(1, "Rutina Piernas");
-
-        dao.crear(r);
-
-        System.out.println("Programa terminado");
+        AppConsola app = new AppConsola();
+        app.iniciar();
 
     }
 
