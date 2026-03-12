b35c2c65076410108b295e283f29d1ed9aa8dc11
arooika4 <aroamilagros2005@gmail.com>
2026-03-12 17:23:47 +0100

    Create Main.java

---
+++ src/main/Main.java
@@ -0,0 +1,13 @@
+package main;
+
+import ui.AppConsola;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        AppConsola app = new AppConsola();
+        app.iniciar();
+
+    }
+}
