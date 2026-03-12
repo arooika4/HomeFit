aa257160b791dbc352be9dc065f6f0423bdaa4fe
arooika4 <aroamilagros2005@gmail.com>
2026-03-12 18:19:35 +0100

    Update EjercicioDAOParse.java

--- src/dao/impl/parse/EjercicioDAOParse.java
+++ src/dao/impl/parse/EjercicioDAOParse.java
@@ -2,46 +2,56 @@ package dao.impl.parse;
 
 import dao.EjercicioDAO;
 import model.Ejercicio;
+import service.Back4AppConnection;
 
-import java.util.List;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
 import java.util.ArrayList;
+import java.util.List;
 
 public class EjercicioDAOParse implements EjercicioDAO {
 
     @Override
     public void crear(Ejercicio ejercicio) {
 
-        System.out.println("Creando ejercicio en Back4App");
+        try {
+
+            HttpURLConnection conn = Back4AppConnection.conectar("Ejercicio", "POST");
+
+            String json =
+                    "{ \"nombre\":\"" + ejercicio.getNombre() + "\"" +
+                    ",\"descripcion\":\"" + ejercicio.getDescripcion() + "\"" +
+                    ",\"videoReferencia\":\"" + ejercicio.getVideoReferencia() + "\"" +
+                    "}";
+
+            OutputStream os = conn.getOutputStream();
+            os.write(json.getBytes());
+            os.flush();
+
+            System.out.println("Ejercicio guardado en Back4App");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 
     @Override
     public Ejercicio obtener(int id) {
-
-        System.out.println("Obteniendo ejercicio");
-
         return null;
     }
 
     @Override
     public List<Ejercicio> obtenerTodos() {
 
-        System.out.println("Listando ejercicios");
+        System.out.println("GET ejercicios");
 
         return new ArrayList<>();
     }
 
     @Override
-    public void actualizar(Ejercicio ejercicio) {
-
-        System.out.println("Actualizando ejercicio");
-
-    }
+    public void actualizar(Ejercicio ejercicio) {}
 
     @Override
-    public void eliminar(int id) {
-
-        System.out.println("Eliminando ejercicio");
-
-    }
+    public void eliminar(int id) {}
 }
