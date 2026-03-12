84a30a81e8cd5483c11eadad4d9d9aa0408513b6
arooika4 <aroamilagros2005@gmail.com>
2026-03-12 17:33:24 +0100

    Create EjercicioDAOCollection.java

---
+++ src/dao/impl/EjercicioDAOCollection.java
@@ -0,0 +1,46 @@
+package daoimpl;
+
+import dao.EjercicioDAO;
+import model.Ejercicio;
+import java.util.ArrayList;
+import java.util.List;
+
+public class EjercicioDAOCollection implements EjercicioDAO {
+
+    private List<Ejercicio> ejercicios = new ArrayList<>();
+
+    @Override
+    public void crear(Ejercicio ejercicio) {
+        ejercicios.add(ejercicio);
+    }
+
+    @Override
+    public Ejercicio obtener(int id) {
+        for (Ejercicio e : ejercicios) {
+            if (e.getIdEjercicio() == id) {
+                return e;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public List<Ejercicio> obtenerTodos() {
+        return ejercicios;
+    }
+
+    @Override
+    public void actualizar(Ejercicio ejercicio) {
+        for (int i = 0; i < ejercicios.size(); i++) {
+            if (ejercicios.get(i).getIdEjercicio() == ejercicio.getIdEjercicio()) {
+                ejercicios.set(i, ejercicio);
+                break;
+            }
+        }
+    }
+
+    @Override
+    public void eliminar(int id) {
+        ejercicios.removeIf(e -> e.getIdEjercicio() == id);
+    }
+}
